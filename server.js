2f478f30c281b011e198ec29b478d4c35ed97168
Leonardo <leonardo.duran.s@mail.pucv.cl>
2024-07-17 20:50:28 -0400

    Primer commit

---
+++ server.js
@@ -0,0 +1,31 @@
+// server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+// const mongoose = require('mongoose');
+
+// Inicializar Express
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// Conectar a MongoDB (cambiar URI por tu conexión a la base de datos)
+// mongoose.connect('mongodb://localhost:27017/express_api', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+// .then(() => console.log('MongoDB connected...'))
+// .catch(err => console.error(err));
+
+// Definir una ruta simple
+app.get('/', (req, res) => {
+  res.send('API funcionando');
+});
+
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
